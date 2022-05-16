89796261da421021409e13a9adb5a4015f8e158d
sapphonie <sappho@sappho.io>
2022-05-16 12:25:43 +0000

    update regensdk

--- regensdk.sh
+++ regensdk.sh
@@ -9,9 +9,12 @@ git clone https://github.com/alliedmodders/hl2sdk --branch sdk2013 --single-bran
 cp ./hl2sdk-2013/lib/public/linux32/libsteam_api.so ./bin/libsteam_api.so
 # Fix conflicting return type with GetPlayerMins/GetPlayerMaxs
 # https://github.com/alliedmodders/hl2sdk/pull/99
-curl https://raw.githubusercontent.com/sapphonie/hl2sdk/8409400b1156289adadb0c8f583a1d9945458523/game/shared/gamemovement.h -o ./hl2sdk-tf2/game/shared/gamemovement.h
+rm  ./hl2sdk-tf2/game/shared/gamemovement.h
+# permalink to this commit
+curl https://raw.githubusercontent.com/sapphonie/hl2sdk/10934b8c9edf900850b1d85f1b9eff90addc0084/game/shared/gamemovement.h -o ./hl2sdk-tf2/game/shared/gamemovement.h
 # Copy steamworks funcs from 2013 to tf2 sdk
 cp ./hl2sdk-2013/public/steam/* ./hl2sdk-tf2/public/steam/ -Rfv
 # Replay stuff for replayserver->GetGameServer() - superceded by engine->GetIServer()
 # cp ./hl2sdk-2013/common/replay/ ./hl2sdk-tf2/common/ -Rfva
+
 rm -rf hl2sdk-2013
